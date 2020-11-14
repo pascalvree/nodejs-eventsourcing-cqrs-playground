33c25592bafa83d26137b322dbc266d240d03a85
Pascal Vree <pascal.vree@rogare.org>
2020-11-14 17:34:01 +0100

    chore(project layout): added a src-folder, renamed main.js to run.js
    Signed-off-by: P. Vree <p.vree@portbase.com>

--- run.js
+++ run.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const repository = require('./repository')();
-const accountAggregate = require('./bankaccount/accountAggregateFactory')(repository);
-
 const eventStream = [
     require('./resources/createdAccountEvent-0.json'),
     require('./resources/withdrewAmountEvent-3.json'),
@@ -10,6 +7,10 @@ const eventStream = [
     require('./resources/depositedAmountEvent-1.json')
 ];
 
+const repository = require('./src/repository')();
+const accountAggregate = require('./src/bankaccount/accountAggregateFactory')(repository);
+
 eventStream.forEach(event => accountAggregate(event));
+
 const account = repository.loadEntryUsingId(eventStream[0].AccountNumber);
 43 === account.Amount ? console.log(account, `PASS: Account with amount of ${account.Amount} matches expectation of 43`) : console.error(account, `FAIL: Account with amount of ${account.Amount} does not match expectation of 43`);
